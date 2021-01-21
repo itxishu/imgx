23d6b846744c98e8f76f0fe8ad0da6e10c2293ad
詹志强 <zqzhan@kaikeba.com>
2021-01-21 14:58:19 +0800

    feat: 添加文档

--- src/utils/withHooksHoc.js
+++ src/utils/withHooksHoc.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const withHooksHoc = (Component) => {
   return (props) => {
-    const screenWidth = useScreenWidth();
-
-    return <Component width={screenWidth} {...props} />;
+    return <Component {...props} />;
   };
 };
 
